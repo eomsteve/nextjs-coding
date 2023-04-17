33539f299d6c14ee1a9bdabcc0c5372b9fa30844
엄성현 <mingyov0807@gmail.com>
2023-04-17 14:58:51 +0900

    image

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,15 +1,16 @@
 export default function List() {
-  let products = ['Tomatoes', 'Pasta', 'Coconut']
+  let products = ['Tomatoes', 'Pasta', 'Coconut'];
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {
-        products.map(product => {
-          return <div className="food">
-          <h4> {product} $40 </h4>
-        </div>
-        })
-      }
+      {products.map((product : string) => {
+        return (
+          <div className="food">
+            <img src="/food0.png" alt="" className="w-[100%] h-[auto]" />
+            <h4> {product} $40 </h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
